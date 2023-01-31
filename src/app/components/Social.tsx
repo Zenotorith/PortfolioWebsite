89d4f6893491bde66a7894f02e6d52d5555f5802
HoangKhoi34 <65653624+Zenotorith@users.noreply.github.com>
2023-02-01 00:42:32 +0700

    first commit

---
+++ src/app/components/Social.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/__vincent.34/" className="home__social-icon">
+                <i className="uil uil-instagram"></i>
+            </a>
+            <a href="https://github.com/Zenotorith" className="home__social-icon">
+                <i className="uil uil-github-alt"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/hoangkhoiduong/" className="home__social-icon">
+                <i className="uil uil-linkedin-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+import Header from './components/Header'
+import Introduction from './components/Introduction'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Introduction />
+    </>
+  )
+}
