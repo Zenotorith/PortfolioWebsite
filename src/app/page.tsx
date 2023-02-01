853334cbc12fed5fe8e0c51b0cc340c6c135e07a
HoangKhoi34 <65653624+Zenotorith@users.noreply.github.com>
2023-02-01 11:50:48 +0700

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
-import Header from './components/Header'
-import Introduction from './components/Introduction'
+import Header from './components/header/Header'
+import Introduction from './components/home/Introduction'
 
 export default function Home() {
   return (
